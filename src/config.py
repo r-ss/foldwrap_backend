eb3abc2442c2e89cc6cd1ead760797316b0bf15a
Ress <mail@ress.ws>
2023-12-25 13:30:43 +0300

    fixed config.py, run mode by env variable #1

--- src/config.py
+++ src/config.py
@@ -1,16 +1,27 @@
 import datetime
-import os
 import pathlib
 import socket
+import os  # used only to get run_mode env variable
 
 from pydantic_settings import BaseSettings
 
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+RUN_MODE = os.getenv("RUN_MODE", "develomplemt")
+
 
 PRODUCTION = False
+if RUN_MODE == "production":
+    PRODUCTION = True
+
+
+"""
+also possible to set run-mode by app's hostname value,
+because in production we have predefined docker container with static hostname
+
 if socket.gethostname() == "foldwrap-backend":
     PRODUCTION = True
+"""
+
 
 BASE_DIR_PATH = pathlib.Path.cwd()
 SRC_DIR_PATH = pathlib.Path(__file__).parent  # path to this file
@@ -22,9 +33,6 @@ if PRODUCTION:
 
 if BASE_DIR_PATH != SRC_DIR_PATH.parent:
     raise "Check directory sctructure in config.py"
-
-
-print("Base dir: ", BASE_DIR)
 
 
 class AppConfig(BaseSettings):
@@ -64,7 +72,7 @@ class AppConfig(BaseSettings):
     TESTING_MODE: bool = False  # Must be set to True only in autotests
     TESTING_ASSETS_PATH: str = str(BASE_DIR_PATH.joinpath("testing_assets"))
 
-    # Define passwords for 2 regular users and 1 admin to check permissions in tests
+    # Define passwords for testusers: 2 regular users and 1 admin
     TESTUSER_ALICE_PASSWORD: str = None  # email: alice@foldwrap.com
     TESTUSER_BOB_PASSWORD: str = None  # email: bob@foldwrap.com
     TESTUSER_JESUS_PASSWORD: str = None  # email: jesus@foldwrap.com
@@ -77,14 +85,10 @@ class AppConfig(BaseSettings):
     DATETIME_FORMAT_HUMAN: str = "%d.%m.%Y %H:%M"
 
     # AUTHENTICATION
-    # AUTH_USERNAME_REGEX: dict = {
-    #     "regex": r"\A[\w\-\.]{3,16}\Z",
-    #     "failmessage": "Username must be at least 3 characters and may contain . - _ chars.",
-    # }  # also can be used as hint
     AUTH_PASSWORD_REGEX: dict = {
         "regex": r"\A[\w\-\.]{6,}\Z",
-        "failmessage": "Password must at least 6 characters and may contain . - _ symbols",
-    }  # also can be used as hint
+        "failmessage": "Password must at least 6 characters and may contain . - _ symbols",  # also can be used as hint
+    } 
     AUTH_HASHING_ALGORITHM: str = "HS256"  # algorithm to encode/decode JWT user tokens
     AUTH_ACCESS_TOKEN_EXPIRATION_TIME: datetime.timedelta = datetime.timedelta(hours=12)
     AUTH_REFRESH_TOKEN_EXPIRATION_TIME: datetime.timedelta = datetime.timedelta(days=30)
@@ -99,10 +103,7 @@ class AppConfig(BaseSettings):
 
     class Config:
         """Loads the dotenv file."""
-
         env_file: str = ".env"
 
 
 config = AppConfig()
-
-# sika
